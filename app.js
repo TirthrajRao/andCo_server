12c91a4a98241b25c4215cd4e66aced008938fd7
vivekbharda <vivekkbharda@gmail.com>
2020-03-25 17:29:34 +0530

    change in app.js

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "")));
+
 
 // API routes initialise
 require("./routes/index")(app);
