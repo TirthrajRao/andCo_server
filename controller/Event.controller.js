b355ebe29bb4c24151ecb2aad7816ab621f2bd2b
vivekbharda <vivekkbharda@gmail.com>
2020-04-03 16:19:24 +0530

    changes in update set price

--- controller/Event.controller.js
+++ controller/Event.controller.js
@@ -133,6 +133,10 @@ module.exports.updateSetPriceOfEvent = (req, res) => {
 	if (req.body.paymentDeadlineTime) updateDetails['paymentDeadlineTime'] = req.body.paymentDeadlineTime
 	if (req.body.bankDetails) updateDetails['bankDetails'] = req.body.bankDetails
 	if (req.body.eventId) updateDetails['eventId'] = req.body.eventId
+	if (req.body.hearAbout) updateDetails['hearAbout'] = req.body.hearAbout
+	if (req.body.payMentTransferDate) updateDetails['payMentTransferDate'] = req.body.payMentTransferDate
+	if (req.body.isLogistics) updateDetails['isLogistics'] = req.body.isLogistics
+	if (req.body.regestery) updateDetails['regestery'] = req.body.regestery
 	console.log("update", updateDetails)
 	eventService.updateSetPrice(updateDetails).then((updated) => {
 		console.log("update completed", updated)
@@ -417,8 +421,14 @@ module.exports.updateGroupInsideActivity = (req, res) => {
  * @returns - MyEventList Or Reason To Fail
  */
 module.exports.MyEventList = (req, res) => {
-	const userId = req.user.user._id;
-	eventService.MyEventList(userId).then((response) => {
+	// const userId = req.user.user._id;
+	let loginUser = req.user
+	if (loginUser.user) {
+		finalId = loginUser.user._id
+	} else if (loginUser.userres) {
+		finalId = loginUser.userres._id
+	}
+	eventService.MyEventList(finalId).then((response) => {
 		return res.status(200).json({ message: response.message, data: response.data });
 	}).catch((error) => {
 		console.error('error: ', error);
